d08394a37cab8ebf9f03dbecbed4f321a0799db5
luo <luokobe@gmail.com>
2013-07-29 17:44:14 +0800

    delete some useless files

--- contentscript.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('mousedown',function(e){
-	if (e.which === 3 && e.target.tagName === "A") {
-		window.templinkTitle = e.target.innerText;
-		console.log(window.templinkTitle);
-		chrome.extension.sendRequest({name:'text',data:templinkTitle});
-	}
-},false);
